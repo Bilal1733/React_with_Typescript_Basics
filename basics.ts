b1f418b57bf7db84a32e6966aa148c632b627a7b
bilals <bks1733@gmail.com>
2023-06-15 19:54:38 +0530

    Exploring the Base Types.

---
+++ basics.ts
@@ -0,0 +1,17 @@
+//primitives: number, string, boolean
+//More complex tytpes:arrays, objects
+//Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
